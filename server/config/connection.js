b6901be6640cb644b8f666176e909264d52f7a27
amiedawn <amie.d.carroll@gmail.com>
2021-03-03 21:06:46 -0700

    heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+console.log("mongodb", process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts' )
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
